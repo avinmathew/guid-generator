bb0e81a697f4d25429089988cca7fa4aace83e2d
Avin Mathew <amathew@assetricity.com>
2012-07-05 16:18:47 +1000

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace GuidGenerator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuid_Click(object sender, RoutedEventArgs e)
+        {
+            Guid guid = Guid.NewGuid();
+            string guidString = guid.ToString();
+            txtGuid.Text = guidString;
+            Clipboard.SetText(guidString);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GUID Generator
+
+This application generates a new GUID based on the .NET Guid class and automatically copies it to the clipboard for easy pasting. The application window also permanently floats above other windows for easy access.
